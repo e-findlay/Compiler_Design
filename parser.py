be5fa61d4c74a6afcb616cf02921daff1dce8d89
e-findlay <44319998+e-findlay@users.noreply.github.com>
2020-03-12 20:12:56 +0000

    updated error checking

--- parser.py
+++ parser.py
@@ -7,15 +7,6 @@ import sys
 
 # need graphviz installed
 
-'''
-TODO
-Check connectives/equality and vars/consts/preds do not share common names
-Check : not in values for input file
-Check forbidden strings for non Terminals vs input terminals
-Check num of parameters for predicates is a positive integer
-Check predicates have []
-'''
-
 def writeToLog(data):
     if os.path.exists('parser.log'):
         mode = 'a'
@@ -50,7 +41,7 @@ def generateGrammar():
         file = sys.argv[1]
     else:
         file = 'file.txt'
-        
+    writeToLog(file + '-Log:')
     with open(file, 'r') as f:
         lines = f.readlines()
         for line in lines:
@@ -60,7 +51,7 @@ def generateGrammar():
                 if len(line) > 2:
                     message = "Error: multiple colons found in line of input file"
                     writeToLog(message)
-                    exit(1)
+                    sys.exit()
                 value = line[1]
                 terms.append(line)
             else:
@@ -83,6 +74,13 @@ def generateGrammar():
         if term[0] == 'formula':
             formula = term[1].split()
 
+    # Check predicates contain [int]
+    for predicate in predicates:
+        if not bool(re.search('\[\d+\]', predicate)):
+            message = "Error: predicate {} does not contain [int]".format(predicate)
+            writeToLog(message)
+            sys.exit()
+
     # Check number of terminals is correct for equality, connectives and quantifiers
     if len(equality) != 1:
         message = "Error: {} equality operators input instead of 1".format(len(equality))
@@ -101,107 +99,79 @@ def generateGrammar():
         sys.exit()
 
     vcp_pattern = re.compile('([A-Z]|[a-z]|[0-9]|_)+')
-    connective_pattern = re.compile('([A-Z]|[a-z]|[0-9]|_|\\\\)+')
-    equality_pattern = re.compile('([A-Z]|[a-z]|[0-9]|_|=|\\\\)+')
+    connective_pattern = re.compile('([A-Z]|[a-z]|[0-9]|_|\\\)+')
+    equality_pattern = re.compile('([A-Z]|[a-z]|[0-9]|_|=|\\\)+')
     
     for variable in variables:
         if not vcp_pattern.match(variable):
-            message = "Error: variable {} can only contain letters, characters and underscores".format(variable)
+            message = "Error: variable {} can only contain letters, numbers and underscores".format(variable)
             writeToLog(message)
-            exit(1)
+            sys.exit()
 
     for constant in constants:
         if not vcp_pattern.match(constant):
-            message = "Error: constant {} can only contain letters, characters and underscores".format(constant)
+            message = "Error: constant {} can only contain letters, numbers and underscores".format(constant)
             writeToLog(message)
-            exit(1)
+            sys.exit()
             
     if not equality_pattern.match(equality[0]):
-        message = "Error: equality {} can only contain letters, characters, _, \\ or =".format(equality[0])
+        message = "Error: equality {} can only contain letters, numbers, _, \\ or =".format(equality[0])
         writeToLog(message)
-        exit(1)
+        sys.exit()
         
     for connective in connectives:
         if not connective_pattern.match(connective):
-            message = "Error: connective {} can only contain letters, characters, _ or \\".format(connective)
+            message = "Error: connective {} can only contain letters, numbers, _ or \\".format(connective)
             print(message)
             writeToLog(message)
-            exit(1)
+            sys.exit()
         
-    non_terminals = ['S', 'Q', 'P', 'E', 'C', 'V', 'J', 'T', 'A']
+    non_terminals = ['<Start>', '<Quantifier>', '<Predicate>', '<Equality>', '<Constant>', '<Variable>', '<Connective>', '<Terminal>', '<Bracketed>']
 
     # Production rules for the CFG accounting for left factoring
     production_rules = {
-        'S' : ['P', '(A', 'QVS', connectives[-1] + 'S'],
-        'A': ['SJS)', 'TET)'],
-        'E': equality,
-        'T': ['C, V'],
-        'Q': quantifiers,
-        'V': variables,
-        'C': constants,
-        'P': [],
-        'J': connectives[:-1]
+        '<Start>' : ['<Predicate>', '(<Bracketed>', '<Quantifier><Variable><Start>', connectives[-1] + '<Start>'],
+        '<Bracketed>': ['<Start><Connective><Start>)', '<Terminal><Equality><Terminal>)'],
+        '<Equality>': equality,
+        '<Terminal>': ['<Constant>, <Variable>'],
+        '<Quantifier>': quantifiers,
+        '<Variable>': variables,
+        '<Constant>': constants,
+        '<Predicate>': [],
+        '<Connective>': connectives[:-1]
     }
 
     predicate_names = []
     predicate_symbol = []
     # Get names of predicate terminals and construct predicate syntax
     for predicate in predicates:
-        idx = re.search(r"\d", predicate).start()
-        idx1 = re.search(r"\d", predicate).end()
-        name = predicate[0:idx-1]
+        idx = re.search(r"\[", predicate).start()
+        idx1 = re.search(r"\]", predicate).end()
+        name = predicate[0:idx]
         predicate_names.append(name + '()')
         predicate_symbol.append(name)
-        num = int(predicate[idx:idx1])
-        atom = name + '(' + 'V,' * (num-1) + 'V' + ')'
-        production_rules['P'].append(atom)
+        num = int(predicate[idx+1:idx1-1])
+        # check parameter number for predicate is positive
+        if num <= 0:
+            message = "Error: predicate must have positive number of parameters"
+            writeToLog(message)
+            sys.exit()
+        atom = name + '(' + '<Variable>,' * (num-1) + '<Variable>' + ')'
+        production_rules['<Predicate>'].append(atom)
         
     # Check predicates, variables and constants do not have any names in common
-    if set(predicate_symbol).intersection(set(constants)):
-        message = "Error: predicates and constants have shared names : {}".format(set(predicate_symbol).intersection(set(constants)))
-        writeToLog(message)
-        sys.exit()
+    commonNames('variables', variables, 'constants', constants)
 
-    if set(predicate_symbol).intersection(set(variables)):
-        message = "Error: predicates and variables have shared names : {}".format(set(predicate_symbol).intersection(set(variables)))
-        writeToLog(message)
-        sys.exit()
+    commonNames('predicates', predicate_symbol, 'constants', constants)
 
-    if set(variables).intersection(set(constants)):
-        message = "Error: variables and constants have shared names : {}".format(set(variables).intersection(set(constants)))
-        writeToLog(message)
-        sys.exit()
+    commonNames('predicates', predicate_symbol, 'variables', variables)
 
-    if set(variables).intersection(set(connectives)):
-        message = "Error: variables and connectives have shared names : {}".format(set(variables).intersection(set(connectives)))
-        writeToLog(message)
-        sys.exit()
+    commonNames('constants', constants, 'connectives', connectives)
 
-    if set(constants).intersection(set(connectives)):
-        message = "Error: constants and connectives have shared names : {}".format(set(constants).intersection(set(connectives)))
-        writeToLog(message)
-        sys.exit()
-
-    if set(predicates).intersection(set(connectives)):
-        message = "Error: predicates and connectives have shared names : {}".format(set(predicates).intersection(set(connectives)))
-        writeToLog(message)
-        sys.exit()
-
-    if set(variables).intersection(set(connectives)):
-        message = "Error: variables and connectives have shared names : {}".format(set(variables).intersection(set(connectives)))
-        writeToLog(message)
-        sys.exit()
-
-    if set(constants).intersection(set(connectives)):
-        message = "Error: constants and connectives have shared names : {}".format(set(constants).intersection(set(connectives)))
-        writeToLog(message)
-        sys.exit()
-
-    if set(predicates).intersection(set(connectives)):
-        message = "Error: predicates and connectives have shared names : {}".format(set(predicates).intersection(set(connectives)))
-        writeToLog(message)
-        sys.exit()
-
+    commonNames('variables', variables, 'connectives', connectives)
+        
+    commonNames('predicates', predicates, 'connectives', connectives)
+    
     commonNames('variables', variables, 'equality', equality)
 
     commonNames('constants', constants, 'equality', equality)
@@ -277,28 +247,29 @@ def generateGrammar():
     print('Non Terminal Symbols: ', ' '.join(non_terminals))
     print('Terminal Symbols: ', ' '.join(equality + connectives + variables + constants + quantifiers + predicate_symbol + ['(', ')', ',']))
     print('Production Rules: ')
-    print('S ->', '|'.join(production_rules['S']))
-    print('A ->', '|'.join(production_rules['A']))
-    print('E ->', '|'.join(production_rules['E']))
-    print('T ->', '|'.join(production_rules['T']))
-    print('Q ->', '|'.join(production_rules['Q']))
-    print('V ->', '|'.join(production_rules['V']))
-    print('C ->', '|'.join(production_rules['C']))
-    print('P ->', '|'.join(production_rules['P']))
-    print('J ->', '|'.join(production_rules['J']))
+    print('<Start> ->', '|'.join(production_rules['<Start>']))
+    print('<Bracketed> ->', '|'.join(production_rules['<Bracketed>']))
+    print('<Equality> ->', '|'.join(production_rules['<Equality>']))
+    print('<Terminal> ->', '|'.join(production_rules['<Terminal>']))
+    print('<Quantifier> ->', '|'.join(production_rules['<Quantifier>']))
+    print('<Variable> ->', '|'.join(production_rules['<Variable>']))
+    print('<Constant> ->', '|'.join(production_rules['<Constant>']))
+    print('<Predicate> ->', '|'.join(production_rules['<Predicate>']))
+    print('<Connective> ->', '|'.join(production_rules['<Connective>']))
+    # write Context Free Grammar to file
     writeGrammar('Grammar For {}'.format(file))
-    writeGrammar('Non Terminal Symbols: ' + ' '.join(non_terminals))
-    writeGrammar('Terminal Symbols: ' + ' '.join(equality + connectives + variables + constants + quantifiers + predicate_symbol + ['(', ')', ',']))
+    writeGrammar('Non Terminal Symbols: ' + ' '.join(non_terminals) + '\n')
+    writeGrammar('Terminal Symbols: ' + ' '.join(equality + connectives + variables + constants + quantifiers + predicate_symbol + ['(', ')', ',']) + '\n')
     writeGrammar('Production Rules:')
-    writeGrammar('S -> ' + '|'.join(production_rules['S']))
-    writeGrammar('A -> ' + '|'.join(production_rules['A']))
-    writeGrammar('E -> ' + '|'.join(production_rules['E']))
-    writeGrammar('T -> ' + '|'.join(production_rules['T']))
-    writeGrammar('Q -> ' + '|'.join(production_rules['Q']))
-    writeGrammar('V -> ' + '|'.join(production_rules['V']))
-    writeGrammar('C -> ' + '|'.join(production_rules['C']))
-    writeGrammar('P -> ' + '|'.join(production_rules['P']))
-    writeGrammar('J -> ' + '|'.join(production_rules['J']))
+    writeGrammar('<Start> -> ' + '|'.join(production_rules['<Start>']))
+    writeGrammar('<Bracketed> -> ' + '|'.join(production_rules['<Bracketed>']))
+    writeGrammar('<Equality> -> ' + '|'.join(production_rules['<Equality>']))
+    writeGrammar('<Terminal> -> ' + '|'.join(production_rules['<Terminal>']))
+    writeGrammar('<Quantifier> -> ' + '|'.join(production_rules['<Quantifier>']))
+    writeGrammar('<Variable> -> ' + '|'.join(production_rules['<Variable>']))
+    writeGrammar('<Constant> -> ' + '|'.join(production_rules['<Constant>']))
+    writeGrammar('<Predicate> -> ' + '|'.join(production_rules['<Predicate>']))
+    writeGrammar('<Connective> -> ' + '|'.join(production_rules['<Connective>']))
 
     # return production_rules, non-terminal symbols and the input formula to be parsed
     return formula, production_rules, non_terminals
@@ -317,22 +288,23 @@ class Parser():
         self.length = len(formula)
         self.nodes = {}
         self.counter = 0
+        self.valid = False
 
     def start(self, parent=None):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('S', parent)
+        self.nodes['var' + str(self.counter)] = Node('<Start>', parent)
         parent = self.nodes['var' + str(self.counter)]
         if self.predicate(parent):
             return True
         parent.children = []
-        if self.lookahead == self.production_rules['S'][-1][:-1]:
-            self.match(production_rules['S'][-1][:-1], parent)
+        if self.lookahead == self.production_rules['<Start>'][-1][:-7]:
+            self.match(production_rules['<Start>'][-1][:-7], parent)
             self.start(parent)
             return True
         parent.children = []
         if self.lookahead == '(':
             self.match('(', parent)
-            self.atom(parent)
+            self.bracketed(parent)
             return True
         parent.children = []
         if self.quantifier(parent):
@@ -343,11 +315,11 @@ class Parser():
             print("Syntax Error")
             message = "Syntax Error: {} at position {} could not be parsed".format(self.lookahead, self.index)
             writeToLog(message)
-            return
+            sys.exit()
 
-    def atom(self, parent):
+    def bracketed(self, parent):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('A', parent)
+        self.nodes['var' + str(self.counter)] = Node('>Bracketed>', parent)
         parent = self.nodes['var' + str(self.counter)]
         if self.term(parent):
             self.equality(parent)
@@ -366,18 +338,18 @@ class Parser():
 
     def equality(self, parent):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('E', parent)
+        self.nodes['var' + str(self.counter)] = Node('<Equality>', parent)
         parent = self.nodes['var' + str(self.counter)]
-        if self.lookahead == self.production_rules['E'][0]:
-            self.match(self.production_rules['E'][0], parent)
+        if self.lookahead == self.production_rules['<Equality>'][0]:
+            self.match(self.production_rules['<Equality>'][0], parent)
             return True
         self.counter -= 1
 
     def variable(self, parent):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('V', parent)
+        self.nodes['var' + str(self.counter)] = Node('<Variable>', parent)
         parent = self.nodes['var' + str(self.counter)]
-        for variable in self.production_rules['V']:
+        for variable in self.production_rules['<Variable>']:
             if self.lookahead == variable:
                 self.match(variable, parent)
                 return True
@@ -385,13 +357,13 @@ class Parser():
 
     def predicate(self, parent):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('P', parent)
+        self.nodes['var' + str(self.counter)] = Node('<Predicate>', parent)
         parent = self.nodes['var' + str(self.counter)]
-        for predicate in self.production_rules['P']:
+        for predicate in self.production_rules['<Predicate>']:
             if self.lookahead == predicate[:predicate.index('(')]:
                 self.match(predicate[:predicate.index('(')], parent)
                 self.match('(', parent)
-                for i in range(predicate.count('V')-1):
+                for i in range(predicate.count('<')-1):
                     self.variable(parent)
                     self.match(',', parent)
                 self.variable(parent)
@@ -402,7 +374,7 @@ class Parser():
 
     def term(self, parent):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('T', parent)
+        self.nodes['var' + str(self.counter)] = Node('<Terminal>', parent)
         parent = self.nodes['var' + str(self.counter)]
         if self.variable(parent):
             return True
@@ -415,9 +387,9 @@ class Parser():
 
     def quantifier(self, parent):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('Q', parent)
+        self.nodes['var' + str(self.counter)] = Node('<Quantifier>', parent)
         parent = self.nodes['var' + str(self.counter)]
-        for quantifier in self.production_rules['Q']:
+        for quantifier in self.production_rules['<Quantifier>']:
             if self.lookahead == quantifier:
                 self.match(quantifier, parent)
                 return True
@@ -426,9 +398,9 @@ class Parser():
     
     def constant(self, parent):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('C', parent)
+        self.nodes['var' + str(self.counter)] = Node('<Constant>', parent)
         parent = self.nodes['var' + str(self.counter)]
-        for constant in self.production_rules['C']:
+        for constant in self.production_rules['<Constant>']:
             if self.lookahead == constant:
                 self.match(constant, parent)
                 return True
@@ -437,9 +409,9 @@ class Parser():
 
     def connective(self, parent):
         self.counter += 1
-        self.nodes['var' + str(self.counter)] = Node('J', parent)
+        self.nodes['var' + str(self.counter)] = Node('<Connective>', parent)
         parent = self.nodes['var' + str(self.counter)]
-        for connective in self.production_rules['J']:
+        for connective in self.production_rules['<Connective>']:
             if self.lookahead == connective:
                 self.match(connective, parent)
                 return True
@@ -462,12 +434,12 @@ class Parser():
             print("Syntax Error")
             message = "Syntax Error: {} at position {} could not be parsed".format(self.lookahead, self.index)
             writeToLog(message)
+            sys.exit()
 
     def nextTerminal(self):
-        print(self.index, self.lookahead)
         self.index += 1
         if self.index == self.length:
-            writeToLog("Success: The Formula: {} is valid".format(' '.join(formula)))
+            self.valid = True
             return
 
         self.lookahead = self.formula[self.index]
@@ -477,6 +449,12 @@ formula, production_rules, non_terminals = generateGrammar()
 parser = Parser(formula, production_rules, non_terminals)
 parser.parse()
 
+# check no characters at end of formula after parsing ends
+if parser.index < parser.length:
+    writeToLog("Error: Additional characters found at the end of formula that were not matched in parsing")
+    sys.exit()
+elif parser.valid:
+    writeToLog("Success: The Formula: {} is valid".format(' '.join(formula)))
 
 # add dot to path
 #c:\Program Files (x86)\Graphviz*\dot.exe on Windows
